59a793ec42e60c75a3130af087d122c0ab8e9678
programmer-joe <66311244+programmer-joe@users.noreply.github.com>
2020-10-24 14:31:22 -0700

    bit of django

---
+++ music/url.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+#/music/
+    url(r'^$',views.index,name = 'index'),
+
+    #/music/71/
+url(r'^(?P<album_id>[0-9]+)/$',views.detail,name = 'detail')
+]
---
+++ music/views.py
@@ -0,0 +1,23 @@
+
+from django.http import HttpResponse
+from django.template import loader
+from .models import Album
+
+def index(request):
+    # all_albums = Album.objects.all()
+    # html = ''
+    # for album in all_albums:
+    #    url ='/music/' + str(album.id)+ '/'
+    #    html += '<a href = "'+ url +'">'+album.album_title+'</a><br>'
+    # return HttpResponse(html)
+    all_albums = Album.objects.all()
+
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context,request))
+
+def detail(request,album_id):
+    return HttpResponse("<h2>Details for album id:"+str(album_id)+"</h2>")
+
